495ac6a0224c9249eb0af9baf5fbd0be06df3bef
Nicolás Scarcella <nscarcella@gmail.com>
2017-03-27 16:45:28 -0300

    Better handling of Proxy not being defined in context

--- src/njsx.js
+++ src/njsx.js
@@ -3,13 +3,16 @@ import {createElement} from 'react'
 const {isArray} = Array
 
 const flatten = (array) => array.reduce((acum, elem) => [...acum, ...isArray(elem) ? elem : [elem]], [])
-const asDynamic = (component) => !Proxy ? component : new Proxy(component, {
-	get(target, name) {
-		const {type, props: {children = [], ...props}} = component()
-		const next = njsx.dynamicSelectorHandler(name, {props, children})
-		return asDynamic( njsx(type, next.props, next.children) )
-	}
-})
+const asDynamic = (component) => {
+	try{ Proxy } catch(error){ return component }
+	return new Proxy(component, {
+		get(target, name) {
+			const {type, props: {children = [], ...props}} = component()
+			const next = njsx.dynamicSelectorHandler(name, {props, children})
+			return asDynamic( njsx(type, next.props, next.children) )
+		}
+	})
+}
 
 export default function njsx(type, props={}, children=[]) {
 	const component = (...args) => {
