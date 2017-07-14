2edb3fd54fd2e76d89c845e215cadc48978cd41a
Nicolás Scarcella <nscarcella@gmail.com>
2017-07-14 14:51:15 -0300

    Fixed bug where null is not ignored for some rule combinations

--- src/rules.js
+++ src/rules.js
@@ -1,64 +1,64 @@
-const {isArray} = Array
+const { isArray } = Array
 
 function mergedStyle(props, style) {
-  if(!props.style && !style) return {}
-  if(!style) return {style: props.style}
+  if (!props.style && !style) return {}
+  if (!style) return { style: props.style }
 
   const newStyle = style.styleId ? style.styleId : style
-  if(!props.style) return {style: newStyle}
-  if(isArray(props.style)) return {style: [...props.style, newStyle]}
-  return {style: [props.style, newStyle]}
+  if (!props.style) return { style: newStyle }
+  if (isArray(props.style)) return { style: [...props.style, newStyle] }
+  return { style: [props.style, newStyle] }
 }
 
 export default {
-  
+
   HASH_AS_ATRIBUTES: {
-    appliesTo(arg) { return typeof arg === 'object' },
-    apply(arg, {props, children}) {
-      return {props: {...props, ...arg, ...mergedStyle(props, arg.style)}, children}
+    appliesTo(arg) { return arg && typeof arg === 'object' },
+    apply(arg, { props, children }) {
+      return { props: { ...props, ...arg, ...mergedStyle(props, arg.style) }, children }
     }
   },
-  
+
   STRING_AS_CLASS: {
     appliesTo(arg) { return typeof arg === 'string' && arg.trim().startsWith('.') },
-    apply(arg, {props: {className = '', otherProps}, children}) { return {props: {...otherProps, className: [...className.split(' '), ...arg.split('.')].map(c=> c.trim()).filter(String).join(' ')} , children } }
+    apply(arg, { props: { className = '', otherProps }, children }) { return { props: { ...otherProps, className: [...className.split(' '), ...arg.split('.')].map(c => c.trim()).filter(String).join(' ') }, children } }
   },
 
   STYLE_AS_STYLE: {
-    appliesTo(arg) { return typeof arg === 'object' && arg.styleId },
-    apply(arg, {props, children}) { return { props:{...props, ...mergedStyle(props,arg)}, children } }
+    appliesTo(arg) { return arg && typeof arg === 'object' && arg.styleId },
+    apply(arg, { props, children }) { return { props: { ...props, ...mergedStyle(props, arg) }, children } }
   },
-  
+
   STRING_AS_CHILD: {
     appliesTo(arg) { return typeof arg === 'string' },
-    apply(arg, {props, children}) { return {props, children: [...children, arg] }}
+    apply(arg, { props, children }) { return { props, children: [...children, arg] } }
   },
-  
+
   NUMBER_AS_CHILD: {
     appliesTo(arg) { return typeof arg === 'number' },
-    apply(arg, {props, children}) { return { props, children: [...children, arg.toString()] }}
+    apply(arg, { props, children }) { return { props, children: [...children, arg.toString()] } }
   },
-  
+
   BOOLEAN_AS_CHILD: {
     appliesTo(arg) { return typeof arg === 'boolean' },
-    apply(arg, {props, children}) { return {props, children: [...children, arg.toString()] }}
+    apply(arg, { props, children }) { return { props, children: [...children, arg.toString()] } }
   },
-  
+
   NJSX_COMPONENT_AS_CHILD: {
     appliesTo(arg) { return typeof arg === 'function' && arg.isNJSXComponent },
-    apply(arg, {props, children}) { return {props, children: [...children, arg()] }}
+    apply(arg, { props, children }) { return { props, children: [...children, arg()] } }
   },
-  
+
   REACT_COMPONENT_AS_CHILD: {
-    appliesTo(arg) { return typeof arg === 'object' && arg.props },
-    apply(arg, {props, children}) { return {props, children: [...children, arg] }}
+    appliesTo(arg) { return arg && typeof arg === 'object' && arg.props },
+    apply(arg, { props, children }) { return { props, children: [...children, arg] } }
   },
-  
+
   IGNORE_NULL: {
     appliesTo(arg) { return arg === null },
     apply(arg, previous) { return previous }
   },
-  
+
   IGNORE_UNDEFINED: {
     appliesTo(arg) { return arg === undefined },
     apply(arg, previous) { return previous }
--- test/njsx.test.js
+++ test/njsx.test.js
@@ -1,17 +1,17 @@
 import { expect, assert } from 'chai'
 import mock from 'mock-require'
-import React, {Component as ReactComponent} from 'react'
+import React, { Component as ReactComponent } from 'react'
 import njsx from '../src/index.js'
 import Rules from '../src/rules.js'
 
-const {keys, assign, defineProperty} = Object
+const { keys, assign, defineProperty } = Object
 
 mock('react-native', {
 	StyleSheet: {
-		create: (styleDefinition) => keys(styleDefinition).reduce((acum, key) => assign(acum, {[key]: 1}), {})
+		create: (styleDefinition) => keys(styleDefinition).reduce((acum, key) => assign(acum, { [key]: 1 }), {})
 	}
 })
-const {StyleSheet} = require("../src/react-native.js")
+const { StyleSheet } = require("../src/react-native.js")
 
 describe('NJSX', () => {
 
@@ -19,35 +19,35 @@ describe('NJSX', () => {
 		it('should be creatable from a type name', () => {
 			const component = njsx('foo')()
 
-			expect(component).to.deep.equal(<foo/>)
+			expect(component).to.deep.equal(<foo />)
 		})
-		
+
 		it('should be creatable from a React component', () => {
-			class Component extends ReactComponent { render() { return <foo/> } }
+			class Component extends ReactComponent { render() { return <foo /> } }
 			const component = njsx(Component)()
 
-			expect(component).to.deep.equal(<Component/>)
+			expect(component).to.deep.equal(<Component />)
 		})
-		
+
 		it('should be creatable from a React functional component', () => {
 			const FunctionalComponent = () => createReactElement('foo')
 			const component = njsx(FunctionalComponent)()
 
-			expect(component).to.deep.equal(<FunctionalComponent/>)
+			expect(component).to.deep.equal(<FunctionalComponent />)
 		})
 
 		it('should be refinable by passing attributes as a hash', () => {
 			njsx.rules = [Rules.HASH_AS_ATRIBUTES]
-			const component = njsx('foo')({bar: 'baz'})()
+			const component = njsx('foo')({ bar: 'baz' })()
 
-			expect(component).to.deep.equal(<foo bar='baz'/>)
+			expect(component).to.deep.equal(<foo bar='baz' />)
 		})
 
 		it('should be refinable by passing a string representing a class name', () => {
 			njsx.rules = [Rules.STRING_AS_CLASS]
 			const component = njsx('foo')('.bar.baz')('.qux')()
 
-			expect(component).to.deep.equal(<foo className="bar baz qux"/>)
+			expect(component).to.deep.equal(<foo className="bar baz qux" />)
 		})
 
 		it('should be refinable by passing a string representing content', () => {
@@ -73,37 +73,44 @@ describe('NJSX', () => {
 
 		it('should be refinable by passing other React components as children', () => {
 			njsx.rules = [Rules.REACT_COMPONENT_AS_CHILD]
-			const component = njsx('foo')(<bar/>,<baz/>)()
+			const component = njsx('foo')(<bar />, <baz />)()
 
-			expect(component).to.deep.equal(<foo><bar/><baz/></foo>)
+			expect(component).to.deep.equal(<foo><bar /><baz /></foo>)
 		})
 
 		it('should be refinable by passing other NJSX components as children', () => {
 			njsx.rules = [Rules.NJSX_COMPONENT_AS_CHILD]
 			const component = njsx('foo')(njsx('bar'), njsx('baz'))()
 
-			expect(component).to.deep.equal(<foo><bar/><baz/></foo>)
+			expect(component).to.deep.equal(<foo><bar /><baz /></foo>)
 		})
 
 		it('should be refinable by passing an array of children', () => {
 			njsx.rules = [Rules.NJSX_COMPONENT_AS_CHILD]
 			const component = njsx('foo')([njsx('bar'), njsx('baz')])()
 
-			expect(component).to.deep.equal(<foo><bar/><baz/></foo>)
+			expect(component).to.deep.equal(<foo><bar /><baz /></foo>)
 		})
 
 		it('should ignore null arguments', () => {
 			njsx.rules = [Rules.IGNORE_NULL]
 			const component = njsx('foo')(null, [null])()
 
-			expect(component).to.deep.equal(<foo/>)
+			expect(component).to.deep.equal(<foo />)
+		})
+
+		it('should ignore null, even if it can be refined by other objects', () => {
+			njsx.rules = [Rules.REACT_COMPONENT_AS_CHILD, Rules.HASH_AS_ATRIBUTES, Rules.STYLE_AS_STYLE, Rules.IGNORE_NULL]
+			const component = njsx('foo')(null, [null])()
+
+			expect(component).to.deep.equal(<foo></foo>)
 		})
 
 		it('should ignore undefined arguments', () => {
 			njsx.rules = [Rules.IGNORE_UNDEFINED]
-			const component = njsx('foo')(undefined,[undefined])()
+			const component = njsx('foo')(undefined, [undefined])()
 
-			expect(component).to.deep.equal(<foo/>)
+			expect(component).to.deep.equal(<foo />)
 		})
 
 		it('should not be refinable by invalid arguments', () => {
@@ -116,14 +123,14 @@ describe('NJSX', () => {
 			njsx.dynamicSelectorHandler = Rules.STRING_AS_CLASS.apply
 			const component = njsx('foo').bar.baz.qux()
 
-			expect(component).to.deep.equal(<foo className="bar baz qux"/>)
+			expect(component).to.deep.equal(<foo className="bar baz qux" />)
 		})
 
 		it('should be refinable by property key accessing if a handler is defined', () => {
 			njsx.dynamicSelectorHandler = Rules.STRING_AS_CLASS.apply
 			const component = njsx('foo')['.bar']['baz qux']()
 
-			expect(component).to.deep.equal(<foo className="bar baz qux"/>)
+			expect(component).to.deep.equal(<foo className="bar baz qux" />)
 		})
 
 		it('should not be refinable by dynamic messages if a handler is not defined', () => {
@@ -139,41 +146,41 @@ describe('NJSX', () => {
 		})
 
 		describe('should be refinable by njsx styles when', () => {
-			const styleSheet = StyleSheet.create({style: {bar: "baz"} })
+			const styleSheet = StyleSheet.create({ style: { bar: "baz" } })
 
 			it('no previous style is defined', () => {
 				njsx.rules = [Rules.STYLE_AS_STYLE]
 				const component = njsx('foo')(styleSheet.style)()
 
-				expect(component).to.deep.equal(<foo style={1}/>)
+				expect(component).to.deep.equal(<foo style={1} />)
 			})
 
 			it('style is already defined as array', () => {
 				njsx.rules = [Rules.STYLE_AS_STYLE, Rules.HASH_AS_ATRIBUTES]
-				const component = njsx('foo')({style: [5]})(styleSheet.style)()
+				const component = njsx('foo')({ style: [5] })(styleSheet.style)()
 
-				expect(component).to.deep.equal(<foo style={[5,1]}/>)
+				expect(component).to.deep.equal(<foo style={[5, 1]} />)
 			})
 
 			it('style is already defined as object', () => {
 				njsx.rules = [Rules.STYLE_AS_STYLE, Rules.HASH_AS_ATRIBUTES]
-				const component = njsx('foo')({style: {bar: "baz"}})(styleSheet.style)()
+				const component = njsx('foo')({ style: { bar: "baz" } })(styleSheet.style)()
 
-				expect(component).to.deep.equal(<foo style={[{bar: "baz"}, 1]}/>)
+				expect(component).to.deep.equal(<foo style={[{ bar: "baz" }, 1]} />)
 			})
 
 			it('style is already defined as id', () => {
 				njsx.rules = [Rules.STYLE_AS_STYLE, Rules.HASH_AS_ATRIBUTES]
-				const component = njsx('foo')({style: 5})(styleSheet.style)()
+				const component = njsx('foo')({ style: 5 })(styleSheet.style)()
 
-				expect(component).to.deep.equal(<foo style={[5,1]}/>)
+				expect(component).to.deep.equal(<foo style={[5, 1]} />)
 			})
 
 			it('style is already defined and a new style is set as hash attribute', () => {
 				njsx.rules = [Rules.STYLE_AS_STYLE, Rules.HASH_AS_ATRIBUTES]
-				const component = njsx('foo')(styleSheet.style)({style: 5})()
+				const component = njsx('foo')(styleSheet.style)({ style: 5 })()
 
-				expect(component).to.deep.equal(<foo style={[1,5]}/>)
+				expect(component).to.deep.equal(<foo style={[1, 5]} />)
 			})
 
 		})
